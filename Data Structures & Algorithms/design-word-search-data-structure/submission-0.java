b17e56b621a0f5a6da1b0e2120b8a760b2ba737d
vaishaliahir <chavdavaishali639@gmail.com>
2026-07-13 21:23:06 +0530

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.java
@@ -0,0 +1,56 @@
+class WordDictionary {
+
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isEnd;
+    }
+
+    private TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+
+    public void addWord(String word) {
+        TrieNode curr = root;
+
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+
+            if (curr.children[index] == null) {
+                curr.children[index] = new TrieNode();
+            }
+
+            curr = curr.children[index];
+        }
+
+        curr.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        return dfs(word, 0, root);
+    }
+
+    private boolean dfs(String word, int index, TrieNode node) {
+        if (node == null) {
+            return false;
+        }
+
+        if (index == word.length()) {
+            return node.isEnd;
+        }
+
+        char c = word.charAt(index);
+
+        if (c == '.') {
+            for (TrieNode child : node.children) {
+                if (child != null && dfs(word, index + 1, child)) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            return dfs(word, index + 1, node.children[c - 'a']);
+        }
+    }
+}
